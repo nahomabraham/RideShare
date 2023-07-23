c8e2dc9dfcf2db384bbf585869524fad771bccee
duyared <84474846+duyared@users.noreply.github.com>
2023-07-23 13:42:02 +0300

    Update list_page.dart

--- ride_share/lib/pages/list_page.dart
+++ ride_share/lib/pages/list_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,7 +19,6 @@ class MyApp extends StatelessWidget {
 
 class MyStatelessWidget extends StatelessWidget {
   const MyStatelessWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
     return DataTable(
